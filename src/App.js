7a170a466d798a2fea652f7dbde6cc201e445521
Divya Yash <divyayashsaxena2000@gmail.com>
2023-01-18 23:44:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import SharedComponent from './components/SharedComponent';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Error404 from './pages/Error404';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Scope from './pages/Scope';
+import Summary from './pages/Summary';
+import Footer from './shared/Footer';
+import Sidebar from './shared/Sidebar';
+import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+
+      <ScrollToTop/>
+    <Routes>
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/" element={<SharedComponent/>} >
+   
+    <Route index element={<Home/>} />
+    <Route path="/home" element={<Home/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/contact" element={<Contact/>} />
+    <Route path="/scope" element={<Scope/>} />
+    <Route path="/summary" element={<Summary/>} />
+
+    <Route path="*" element={<Error404/>} />
+
+
+    </Route>
+   </Routes>
+       </Router>
+       </>
   );
 }
 
